1c73e25329c6ef9682c91ef179b2fd0ee1a0ec9d
Juan Pinheiro <jtlpinheiro@gmail.com>
2019-08-30 14:27:12 -0300

    on center

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -4,9 +4,23 @@ import WorldMap from 'components/WorldMap';
 
 export const Root = () => {
     return (
-        <>
-            <Player />
-            <WorldMap />
-        </>
+        <div style={{
+            width: '100%',
+            height: '100vh',
+            minWidth: 320,
+            minHeight: 320,
+            alignItems: 'center',
+            justifyContent: 'center',
+            display: 'flex'
+        }}>
+            <div style={{
+                width: 320,
+                height: 320,
+                position: 'relative'
+            }}>
+                <Player />
+                <WorldMap />
+            </div>
+        </div>
     );
 }
